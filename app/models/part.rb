d5c5d04e94c7758dbbaa61831d372bc7d2bf8f5a
y1220 <ga.2.y1220@gmail.com>
2020-11-20 17:42:31 +0100

    completed till the chapter 5(scope, visibility, self)

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,8 @@
 class Part < ApplicationRecord
   has_many :chapters, dependent: :destroy
   has_many :pages, through: :chapters, dependent: :destroy
+  # attr_accessor :name
+  # def initialize(name)
+  #   @name = name
+  # end
 end
